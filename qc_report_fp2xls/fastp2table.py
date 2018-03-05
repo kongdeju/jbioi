e1c8e01924c093ab1ac880ece7127d3b7a3d3249
KongDeju <kongdeju@gene.ac>
2018-03-05 16:55:27 +0800

    fp2xls

---
+++ qc_report_fp2xls/fastp2table.py
@@ -0,0 +1,50 @@
+
+import json
+import os
+from jbiot import log
+from jbiot import jbiotWorker
+
+
+def fastp2table(jsons):
+    '''convert jsons to tsv
+
+    Args:
+        jsons (list) :  has `jsons`
+
+    Returns:
+        dict: fastpTable 
+
+    '''
+
+    out = "fastqInfo.tsv" 
+    fpw = open(out,"w")
+    head = "\t".join(["sample","total_reads","total_bases","q20_rate","q30_rate","gc_content"]) + "\n"
+    fpw.write(head)
+    for js in jsons:
+        sample = js.rstrip(".json")
+        sample = sample.split("/")[-1]
+        fp = open(js)
+        jd = json.loads(fp.read())
+        info = jd["summary"]["before_filtering"]
+        reads = info["total_reads"]
+        bases = info["total_bases"]
+        q20 = info["q20_rate"]
+        q30 = info["q30_rate"]
+        gc = info["gc_content"]
+        line = "\t".join( [str(item) for item in [sample,reads,bases,q20,q30,gc]]) + "\n"
+        fpw.write(line)
+    fpw.close()
+
+    return out
+
+
+class fastp2tableWorker(jbiotWorker):
+    def handle_task(self, key, params):
+        self.execMyfunc(fastp2table,params)
+
+if __name__ == "__main__":
+    import sys
+    js = sys.argv[1:]
+    fastp2table(js)
+
+

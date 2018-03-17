84722932fc82d0424a1f2bf4a0f9b39fd1704c77
KongDeju <kongdeju@gene.ac>
2018-03-17 22:45:21 +0800

    varscan gatk samtools maftools fastqc bowtie2

---
+++ maftools-master/install.r
@@ -0,0 +1,7 @@
+source("http://bioconductor.org/biocLite.R");
+biocLite("ComplexHeatmap") ; 
+biocLite("VariantAnnotation"); 
+biocLite("Biostrings")
+install.packages("devtools")
+library("devtools");
+install_github(repo = "PoisonAlien/maftools")
